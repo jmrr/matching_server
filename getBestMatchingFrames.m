7ed01c524053449c40b8f3ee4898c2bb2579be1d
Jose Rivera <jose.rivera@imperial.ac.uk>
2015-02-17 18:04:58 +0000

    Working in MATLAB version matching against only ONE database kernel

--- getBestMatchingFrames.m
+++ getBestMatchingFrames.m
@@ -1,34 +1,24 @@
 function bestMatchingFrames = getBestMatchingFrames(paramsDataset, paramsQuery, queryFrame) %#codegen
 
-
 passes      = paramsDataset.passes;
 trainingSet = passes(1:paramsQuery.queryPass-1,paramsQuery.queryPass+1:end);
-
 numTopMatches = 1; % Only top match for now
-numKernels = length(trainingSet);
-top        = zeros(numKernels,numTopMatches);
-topIdx     = zeros(size(top));
 
 
-for i = 1:numKernels
-    
-    %     kernelStr = sprintf(paramsDataset.kernelPath,paramsQuery.queryCorridor,trainingSet(i),paramsQuery.queryPass);
-    kernelMatFileName = ['C2_db' convertnum(trainingSet(i)) '_q' convertnum(paramsQuery.queryPass)];
-    kernelStr = ['data/'  kernelMatFileName];
-    
-    
-    kernelStruct = load(kernelStr); % load kernel in the variable 'kernel'
-    
-    kernel = kernelStruct.kernel;
+kernelMatFileName = ['C2_db' convertnum(trainingSet(1)) '_q' convertnum(paramsQuery.queryPass)]; % HARDCODED 1
+kernelFileName = ['data/'  kernelMatFileName];
 
-    
-    scores          = kernel(queryFrame,:);
-    [sortedScr, idx] = sort(scores,'descend');
-    top(i,:)        = sortedScr(1:numTopMatches);
-    topIdx(i,:)     = idx(1:numTopMatches);
-    
+kernelStruct   = load(kernelFileName);
+
+kernel = kernelStruct.kernel;
+
+scores = kernel(queryFrame,:);
+[sortedScr, idx] = sort(scores,'descend');
+
+top        = sortedScr(1:numTopMatches);
+topIdx     = idx(1:numTopMatches);
+
+bestMatchingFrames = topIdx;
+
+
 end
-
-bestMatchingFrames = [trainingSet; topIdx(:,1)']; % First row for db pass
-
-end%     
